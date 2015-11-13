09b20a651a2712d55512303360e42252e73e9adf
Marius Hårstad Kjerkreit <mkjerkreit@gmail.com>
2015-11-13 10:46:00 +0100

    decorator that ensures a live connection to mpd included, minor tweaks

--- mpd_conn.py
+++ mpd_conn.py
@@ -3,31 +3,50 @@ from mpd import MPDClient
 from config import MPD_SRV
 
 srv = MPDClient()
+volume = 76
 
-#
-# skal bli decorator
-#
-srv.connect(MPD_SRV['host'], MPD_SRV['port'])
-srv.password(MPD_SRV['passwd'])
-srv.repeat(1)
-#srv.close()
-#
+def conn(func):
+    def wrapper(*args):
+        srv.connect(MPD_SRV['host'], MPD_SRV['port'])
+        srv.password(MPD_SRV['passwd'])
+        func(*args)
+        srv.disconnect()
 
-#volume = srv.status()['volume']
+    return wrapper
 
+@conn
+def init():
+    global volume
+    srv.repeat(1)
+    srv.setvol(volume)
+
+@conn
 def dec_vol():
-    #volume = volume - 3
-    srv.setvol(int(srv.status()['volume']) - 3)
+    global volume
+    if volume > 50:
+        volume -= 2
+        srv.setvol(volume)
+        print "volume dereased to " + str(volume)
+    else:
+        print "volume at minimum"
 
+@conn
 def inc_vol():
-    #volume = volume + 3
-    srv.setvol(int(srv.status()['volume']) + 3)
+    global volume
+    if volume < 100:
+        volume += 2
+        srv.setvol(volume)
+        print "volume inreased to " + str(volume)
+    else:
+        print "volume at maximum"
 
+@conn
 def play(playlist):
     srv.clear()
     srv.load(playlist)
     srv.play(0)
 
+@conn
 def stop():
     srv.stop()
     srv.clear()
